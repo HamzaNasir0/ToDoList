4fc5942a3b7b59ffa188b6846dfec38c41063f29
HamzaNasir0 <hamza.nasir2@Gmail.com>
2025-10-09 12:49:06 +0100

    added basic html, js and css boilerplate

---
+++ assets/main.js
@@ -0,0 +1,5 @@
+// Global variables for DOM elements
+const taskInput = document.getElementById('taskInput');
+const addTaskBtn = document.getElementById('addTaskBtn');
+const taskList = document.getElementById('taskList');
+
