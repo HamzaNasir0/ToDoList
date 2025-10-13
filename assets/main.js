dc4d89484d1ec0c57c2cf81f0a0bde6d7c30f74a
HamzaNasir0 <hamza.nasir2@Gmail.com>
2025-10-13 14:29:56 +0100

    updated the styling

--- assets/main.js
+++ assets/main.js
@@ -3,3 +3,36 @@ const taskInput = document.getElementById('taskInput');
 const addTaskBtn = document.getElementById('addTaskBtn');
 const taskList = document.getElementById('taskList');
 
+addTaskBtn.addEventListener('click', addTask);
+
+taskInput.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
+        addTask();
+    }
+});
+
+function addTask() {
+    const taskText = taskInput.value.trim();
+
+
+    if (taskText === "") {
+        alert("Task cannot be empty. Please enter a valid task.");
+        return;
+    }
+
+    const existingTasks = Array.from(taskList.querySelectorAll('.task-content')).map(el => el.textContent.toLowerCase());
+    if (existingTasks.includes(taskText.toLowerCase())) {
+        alert(`The task "${taskText}" already exists.`);
+        taskInput.value = ''; // Clear input on failed validation
+        return;
+    }
+
+    const taskItem = createTaskElement(taskText);
+
+    taskList.appendChild(taskItem);
+
+    taskInput.value = '';
+}
+
+
+PromiseRejectionEvent.alert
